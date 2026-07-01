f7e096d7e7928eba70fa1fbea7e60ddd6d785530
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2026-07-01 18:09:37 +0200

    prepare deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/kniffel/",
   test: {
     globals: false,
   },
-})
+});
